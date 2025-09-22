d9ff6d47d20506120406a6c823dc71dad595156f
ahmedaryf <ahmed.areef@hotmail.com>
2025-09-22 22:57:40 +0500

    title moved to bottom of the image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroSection from "./components/HeroSection";
+// import HeroSection from "./components/HeroSection";
 import { client } from "@/sanity/lib/client";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
@@ -7,14 +7,14 @@ import HeroVideoComponent from "./components/HeroVideoComponent";
 
 export const revalidate = 60;
 
-async function getHeroData() {
-  const query = `*[_type == "hero"]{
-  image,
-  title
-  }`;
-  const data = await client.fetch(query, {}, { next: { revalidate: 60 } });
-  return data;
-}
+// async function getHeroData() {
+//   const query = `*[_type == "hero"]{
+//   image,
+//   title
+//   }`;
+//   const data = await client.fetch(query, {}, { next: { revalidate: 60 } });
+//   return data;
+// }
 
 async function getProperties() {
   const query = `*[_type == "properties"] | order(propertyName asc){
@@ -27,7 +27,7 @@ async function getProperties() {
 }
 
 export default async function page() {
-  const heroData = await getHeroData();
+  // const heroData = await getHeroData();
   const properties = await getProperties();
 
   return (
@@ -40,30 +40,32 @@ export default async function page() {
         </h1>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8 mt-12 md:mt-24 px-6 md:px-24'>
           {properties.map((property: any, index: number) => (
-            <div key={index} className='bg-gray-100'>
-              <h2 className='text-3xl md:text-4xl font-thin text-center mb-2 mt-2'>
-                {property.propertyName}
-              </h2>
-              <div className='bg-amber-200 '>
-                {
-                  <Image
-                    src={urlFor(property.coverImage)}
-                    alt='Cover Image'
-                    width={200}
-                    height={200}
-                    className='aspect-[16/9] object-cover w-full'
-                  />
-                }
+            <div
+              key={index}
+              className='bg-gray-100 flex flex-col gap-4 justify-between rounded'>
+              <div className=' '>
+                <div className=' '>
+                  {
+                    <Image
+                      src={urlFor(property.coverImage)}
+                      alt='Cover Image'
+                      width={200}
+                      height={200}
+                      className='aspect-[16/9] object-cover w-full rounded-t'
+                    />
+                  }
+                </div>
+                <div className='p-2 md:p-4 '>
+                  <h2 className='text-2xl md:text-3xl font-thin text-center mb-2 mt-2'>
+                    {property.propertyName}
+                  </h2>
+                  <h6 className='text-sm'>{property.shortDescription}</h6>
+                </div>
               </div>
-              <div className='flex flex-col justify-between'>
-                <div className='p-2 md:p-4'>
-                  <h6>{property.shortDescription}</h6>
-                </div>
-                <div>
-                  <button className='bg-gray-500 px-2 py-1 w-full text-white mt-auto'>
-                    Learn more
-                  </button>
-                </div>
+              <div>
+                <button className='bg-gray-500 px-2 py-1 w-full text-white mt-auto'>
+                  Learn more
+                </button>
               </div>
             </div>
           ))}
